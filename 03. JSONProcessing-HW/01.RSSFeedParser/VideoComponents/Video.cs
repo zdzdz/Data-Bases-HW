a177e1d74978e183b6c0d34bd940eef0d70a1ae4
zdzdz <zdzdz.link@yahoo.com>
2015-10-07 14:04:27 +0300

    Reupload of JSON Processing Homework

---
+++ 03. JSONProcessing-HW/01.RSSFeedParser/VideoComponents/Video.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.RSSFeedParser.VideoComponents
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    public class Video
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("link")]
+        public Link Link { get; set; }
+
+        [JsonProperty("yt:channelId")]
+        public string Id { get; set; }
+    }
+}
